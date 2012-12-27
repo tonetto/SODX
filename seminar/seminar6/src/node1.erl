41f37e56055ef3e6e4d58f440a1cd10fbdadb795
Leonardo Tonetto <leo.tonetto@gmail.com>
2012-12-27 01:16:28 +0100

    Some more modifications
    It's time to try it out now. Will do it soon. Gotta grab something
    to eat. :-)

--- seminar/seminar6/src/node1.erl
+++ seminar/seminar6/src/node1.erl
@@ -1,6 +1,42 @@
 -module(node1).
+-export([start/1,start/2]).
+
+-ifdef(debug_client).
+-define(DBG(X,Y,Z), io:format("[NODE_DEBUG] ~w: ~s ~w~n", [X, Y, Z])).
+-else.
+-define(DBG(X,Y,Z), true).
+-endif.
 
 -define(Stabilize, 1000).
+-define(Timeout, 5000).
+
+start(MyKey) ->
+    ?DBG(MyKey,"Starting as the first in the ring",start),
+    start(MyKey, nil).
+
+start(MyKey, PeerPid) ->
+    ?DBG(MyKey,"Starting with an existing node:",PeerPid),
+    timer:start(),
+    spawn(fun() -> init(MyKey, PeerPid) end).
+
+init(MyKey, PeerPid) ->
+    Predecessor = nil,
+    {ok, Successor} = connect(MyKey, PeerPid),
+    schedule_stabilize(),
+    node(MyKey, Predecessor, Successor).
+
+connect(MyKey, nil) ->
+    {ok, {MyKey , self()}}; %% TODO
+
+connect(_, PeerPid) ->
+    Qref = make_ref(),
+    PeerPid ! {key, Qref, self()},
+    receive
+        {Qref, Skey} ->
+            {ok, {Skey, PeerPid}} %% TODO
+    after ?Timeout ->
+            io:format("Timeout: no response from ~w~n", [PeerPid])
+    end.
 
 schedule_stabilize() ->
     timer:send_interval(?Stabilize, self(), stabilize).
@@ -20,7 +56,20 @@ node(MyKey, Predecessor, Successor) ->
             Succ = stabilize(Pred, MyKey, Successor),
             node(MyKey, Predecessor, Succ);
         stabilize ->
+            ?DBG(MyKey,"Starting a new Stabilize",stabilize),
             stabilize(Successor),
+            node(MyKey, Predecessor, Successor);
+        probe ->
+            ?DBG(MyKey,"Starting a probe.",probe),
+            create_probe(MyKey, Successor),
+            node(MyKey, Predecessor, Successor);
+        {probe, MyKey, Nodes, T} ->
+            ?DBG(MyKey,"Got my probe back. Ring should be ok", probe),
+            remove_probe(MyKey, Nodes, T),
+            node(MyKey, Predecessor, Successor);
+        {probe, RefKey, Nodes, T} ->
+            ?DBG(MyKey,"Got another probe, forwarding",probe),
+            forward_probe(RefKey, [MyKey|Nodes], T, Successor),
             node(MyKey, Predecessor, Successor);
         _ ->
             node(MyKey, Predecessor, Successor)
@@ -58,3 +107,31 @@ request(Peer, Predecessor) ->
         {Pkey, Ppid} ->
             Peer ! {status, {Pkey, Ppid}}
     end.
+
+notify({Nkey, Npid}, MyKey, Predecessor) ->
+    case Predecessor of
+        nil ->
+            ?DBG(MyKey,"[Notify] New Predecessor:",Nkey),
+            {Nkey, Npid}; %% TODO
+        {Pkey, _} ->
+            case key:between(Nkey, Pkey, MyKey) of
+                true ->
+                    ?DBG(MyKey,"[Notify] New Predecessor:",Nkey),
+                    {Nkey, Npid}; %% TODO: ADD SOME CODE
+                false ->
+                    ?DBG(MyKey,"[Notify] Kept existing predecessor:",Nkey),
+                    Predecessor
+            end
+    end.
+
+create_probe(MyKey, {_, Spid}) ->
+    Spid ! {probe, MyKey, [MyKey], erlang:now()},
+    io:format("Create probe ~w!~n", [MyKey]).
+
+remove_probe(MyKey, Nodes, T) ->
+    Time = timer:now_diff(erlang:now(), T),
+    io:format("Received probe ~w in ~w ms Ring: ~w~n", [MyKey, Time, Nodes]).
+
+forward_probe(RefKey, Nodes, T, {_, Spid}) ->
+    Spid ! {probe, RefKey, Nodes, T},
+    io:format("Forward probe ~w!~n", [RefKey]).
